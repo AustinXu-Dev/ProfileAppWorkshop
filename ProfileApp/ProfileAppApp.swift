9897e04afc353f7d5828ff0843453d880a0bd062
Skypoppi <austin.xu.me@gmail.com>
2024-02-17 09:41:23 +0700

    Initial Commit

---
+++ ProfileApp/ProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppApp.swift
+//  ProfileApp
+//
+//  Created by Austin Xu on 2024/2/17.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
