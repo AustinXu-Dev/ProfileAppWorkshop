1b13647c91e3825f47a8615421eccb7bf0a4a933
Skypoppi <austin.xu.me@gmail.com>
2024-02-17 16:01:48 +0700

    first commit

---
+++ ProfileApp/StateView.swift
@@ -0,0 +1,18 @@
+//
+//  StateView.swift
+//  ProfileApp
+//
+//  Created by Austin Xu on 2024/2/17.
+//
+
+import SwiftUI
+
+struct StateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateView()
+}
